ad5580206b00b96f5440b574dd085864b4ae5d2f
wuchangming <wuchangmingnice@163.com>
2018-07-02 21:47:40 +0800

    add lerna

---
+++ design.md
@@ -0,0 +1,14 @@
+# just-mock
+加速本地开发
+
+## 接口数据存储
+数据结构, 文件结构, 文件名结构  
+要求: 可读性, 方便文件合并  
+库: JSON5, prettier
+
+## 控制台
+库: Electron
+要求: 支持restful, 支持收藏组
+
+## 拦截脚本
+ajax, fetch
--- packages/inspect-script/src/inspect/wrapper/XMLHttpRequestWrapper.js
+++ packages/inspect-script/src/inspect/wrapper/XMLHttpRequestWrapper.js
@@ -1,18 +1,26 @@
 import { WRAPPED_FLAG } from '../constants'
 import nomalizeUrl from '../utils/nomalizeUrl'
 
+// Inspired by:
+// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+// https://github.com/nuysoft/Mock/blob/refactoring/src/mock/xhr/xhr.js
+
 export default mockServerUrl => {
-    var xhr = window.XMLHttpRequest
-    if (xhr && !xhr[WRAPPED_FLAG]) {
-        var open = xhr.prototype.open
-        xhr.prototype.open = function() {
-            const originUrl = arguments[1]
-            arguments[1] = `${mockServerUrl}/mock?originUrl=${encodeURIComponent(
-                nomalizeUrl(originUrl)
-            )}`
-            open.apply(this, arguments)
-        }
-        window.XMLHttpRequest = xhr
-        window.XMLHttpRequest[WRAPPED_FLAG] = true
-    }
+    const xhr = window.XMLHttpRequest
+    
+    // if (xhr && !xhr[WRAPPED_FLAG]) {
+    //     window.justMockBackup = {
+    //         realXHR: xhr
+    //     }
+    //     const open = xhr.prototype.open
+    //     // XMLHttpRequest.open(method, url, async, user, password)
+    //     xhr.prototype.open = function() {
+    //         const originUrl = arguments[1]
+    //         arguments[1] = `${mockServerUrl}/mock?originUrl=${encodeURIComponent(
+    //             nomalizeUrl(originUrl)
+    //         )}`
+    //         open.apply(this, arguments)
+    //     }
+    //     xhr[WRAPPED_FLAG] = true
+    // }
 }
