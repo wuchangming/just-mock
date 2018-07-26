9ffe2cb053bae22e7b7736b93a53c214b94bdfb6
wuchangming <wuchangmingnice@163.com>
2018-07-26 16:18:08 +0800

    delete unused code

--- packages/inspect-script/src/inspect/index.js
+++ packages/inspect-script/src/inspect/index.js
@@ -1,25 +1,8 @@
 import 'babel-polyfill'
-import './wrapper/fetchWrapper'
-import XMLHttpRequestWrapper from './wrapper/XMLHttpRequestWrapper'
 import JustMockXHR from './wrapper/JustMockXHR'
 import { backupXHR } from './utils/backupXHR'
 
-/*
-    PhantomJS
-    TypeError: '[object EventConstructor]' is not a constructor (evaluating 'new Event("readystatechange")')
-    https://github.com/bluerail/twitter-bootstrap-rails-confirm/issues/18
-    https://github.com/ariya/phantomjs/issues/11289
-*/
-try {
-    new window.Event('custom')
-} catch (exception) {
-    window.Event = function(type, bubbles, cancelable, detail) {
-        var event = document.createEvent('CustomEvent') // MUST be 'CustomEvent'
-        event.initCustomEvent(type, bubbles, cancelable, detail)
-        return event
-    }
-}
-// 备份原生 XMLHttpRequest
+// backuo XMLHttpRequest
 backupXHR()
 
 window.XMLHttpRequest = JustMockXHR
